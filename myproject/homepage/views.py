c7d4d5c1b804a1a25c94863ec7e055f003994b76
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-09 17:06:11 +0300

    Import require_GET in views.py

--- myproject/homepage/views.py
+++ myproject/homepage/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-
+from django.views.decorators.http import require_http_methods, require_GET
 from django.http import HttpResponse
 
-
+@require_http_methods(["GET", "POST"])
 def index_page(request):
+    if request.method == "GET":
+        print("Method is GET")
     response = render(request, 'homepage/index.html')
     response['MyCustomHeader'] = 'spam-and-eggs'
     return response
 
+@require_GET
 def articles(request):
     args = {
         'articles': list(range(1, 8)),
