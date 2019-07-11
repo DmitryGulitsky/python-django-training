e5315a940108141c4117dcf979ad7d184ad5c7b4
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-12 00:58:47 +0300

    Add AuthorsModel to models. Call 'makemigrations' in console

--- myproject/authors/models.py
+++ myproject/authors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AuthorsModel(models.Model):
+    class Meta:
+        verbose_name = 'AuthorsModel'
+        verbose_name_plural = 'AuthorsModels'
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
