cc9774574e258f73bf3f3ba99f55a5112e73c931
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-12 23:11:40 +0300

    Import 'ListView' in authors views.py. Show database data on the authors page

--- myproject/authors/views.py
+++ myproject/authors/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
+from .models import AuthorsModel
 
 # Create your views here.
 
-class IndexView(TemplateView):
+
+class IndexView(ListView):
+
+    model = AuthorsModel
     template_name = 'authors/index.html'
+    context_object_name = 'authors'
+
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    context['authors'] = AuthorsModel.objects.all()
+    #    return context
