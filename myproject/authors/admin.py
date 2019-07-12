618345bfb00497f911c202711bddd1f88e576aff
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-12 20:53:50 +0300

    Modify model fields. Add default values for empty states

--- myproject/authors/admin.py
+++ myproject/authors/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 
 from .models import AuthorsModel
 
+
+@admin.register(AuthorsModel)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', )
+    list_display = ('id', '__str__', 'email_domain', 'abc')
 
+    empty_value_display = '(email not stated)'
 
-admin.site.register(AuthorsModel, AuthorAdmin)
+    def abc(self, object):
+        return
+
+    def email_domain(self, obj):
+        if obj.email is not None:
+            return obj.email.partition('@')[2]
+
+    email_domain.empty_value_display = '[email not stated]'
+
+#admin.site.register(AuthorsModel, AuthorAdmin)
