651417965041064fe149eb5a03a7437927c46938
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-12 15:08:03 +0300

    Add data to authors

--- myproject/authors/models.py
+++ myproject/authors/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class AuthorsModel(models.Model):
     class Meta:
-        verbose_name = 'AuthorsModel'
-        verbose_name_plural = 'AuthorsModels'
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email_address = models.EmailField()
+    email = models.EmailField(null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
