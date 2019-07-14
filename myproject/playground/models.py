c175ff2cc99b900348808371c7d3b2816342f6b3
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-14 16:10:37 +0300

    Add Student and StudentInformation to playground models

--- myproject/playground/models.py
+++ myproject/playground/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class StudentInformation(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    pass_id = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Student(models.Model):
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
