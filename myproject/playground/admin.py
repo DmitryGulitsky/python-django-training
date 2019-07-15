f1b732c67193a0162efb3276e4b74bded8a95b70
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-07-16 01:01:25 +0300

    Import playground models in admin.py. Add data to database

--- myproject/playground/admin.py
+++ myproject/playground/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, StudentInfo, \
+    Publisher, Article
+
+admin.site.register(Student)
+admin.site.register(StudentInfo)
+admin.site.register(Publisher)
+admin.site.register(Article)
+
